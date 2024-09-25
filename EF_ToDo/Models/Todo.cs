af7e3562a68719e2f61aeb22b0d6c1d553cea96c
apautrat17 <anthony.ptrt@gmail.com>
2024-09-25 15:47:43 +0200

    TP1

--- EF_ToDo/Models/Todo.cs
+++ EF_ToDo/Models/Todo.cs
@@ -3,9 +3,6 @@ public class Todo{
     public string? Task{get;set;}
     public bool Completed{get;set;}
 
-    public Todo(int id, string tache, bool completed){
-        Id=id;
-        Task=tache;
-        Completed=completed;
+    public Todo(){
     }
 }
--- EF_ToDo/Program.cs
+++ EF_ToDo/Program.cs
@@ -1,3 +1,48 @@
-﻿Todo t1 = new Todo(0, "hnuxagrf", false);
-Todo t2 = new Todo(1, "fuyebczygfnu", true);
-Todo t3 = new Todo(2, "'ueuyzgh'", false);
+﻿
+
+void Ajouter(string task, bool complete){
+    using (var context = new TodoContext()){
+            var todo1 = new Todo{
+            Task = task,
+            Completed = complete
+            };
+            context.Todo.Add(todo1);
+            context.SaveChanges();
+        }
+}
+
+// Ajouter("Passer la tondeuse (encore)", false);
+
+
+void Supprimer(int id){
+    using (var context = new TodoContext()){
+            var tododel = context.Todo.Find(id);
+        if (tododel != null){
+            context.Todo.Remove(tododel); 
+            context.SaveChanges();
+        }
+    }
+}
+
+// Supprimer(3);
+
+
+void LireTodos(){
+    using (var context = new TodoContext()){
+        var todos = context.Todo.ToList();  // Récupérer tous les éléments
+        foreach (Todo todo in todos){
+            Console.WriteLine($"Id = {todo.Id}, Task = {todo.Task},  State = {todo.Completed}");
+        }
+    }
+}
+
+LireTodos();
+
+    
+
+
+    
+
+
+
+
